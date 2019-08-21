a95d0843c7575b4fd96f954287fb1a78fd7c2584
xiaoke.luo <wtimexiaoke@gmail.com>
2019-08-21 16:10:30 +0800

    update

--- NDKDemo/app/src/main/java/com/lxk/ndkdemo/JniBitmapDemo.java
+++ NDKDemo/app/src/main/java/com/lxk/ndkdemo/JniBitmapDemo.java
@@ -12,31 +12,29 @@ import java.nio.ByteBuffer;
  * <p>
  * jni使用bitmap
  */
-public class JniBitmapDemo {
+    public class JniBitmapDemo {
 
-    private static final String TAG = "JniBitmapDemo";
+        private static final String TAG = "JniBitmapDemo";
 
-    static {
-        System.loadLibrary("bitmap");
+        static {
+            System.loadLibrary("bitmap");
+        }
+
+        public native void passBitmap(Bitmap bitmap);
+
+
+        public void test() {
+            Bitmap bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+            bitmap.eraseColor(0xff336699); // AARRGGBB
+            byte[] bytes = new byte[bitmap.getWidth() * bitmap.getHeight() * 4];
+            Buffer dst = ByteBuffer.wrap(bytes);
+            bitmap.copyPixelsToBuffer(dst);
+            // ARGB_8888 真实的存储顺序是 R-G-B-A
+            Log.d(TAG, "R: " + Integer.toHexString(bytes[0] & 0xff));
+            Log.d(TAG, "G: " + Integer.toHexString(bytes[1] & 0xff));
+            Log.d(TAG, "B: " + Integer.toHexString(bytes[2] & 0xff));
+            Log.d(TAG, "A: " + Integer.toHexString(bytes[3] & 0xff));
+
+            passBitmap(bitmap);
+        }
     }
-
-    public native void passBitmap(Bitmap bitmap);
-
-
-    public void test() {
-        Bitmap bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
-        bitmap.eraseColor(0xff336699); // AARRGGBB
-        byte[] bytes = new byte[bitmap.getWidth() * bitmap.getHeight() * 4];
-        Buffer dst = ByteBuffer.wrap(bytes);
-        bitmap.copyPixelsToBuffer(dst);
-        // ARGB_8888 真实的存储顺序是 R-G-B-A
-        Log.d(TAG, "R: " + Integer.toHexString(bytes[0] & 0xff));
-        Log.d(TAG, "G: " + Integer.toHexString(bytes[1] & 0xff));
-        Log.d(TAG, "B: " + Integer.toHexString(bytes[2] & 0xff));
-        Log.d(TAG, "A: " + Integer.toHexString(bytes[3] & 0xff));
-
-        passBitmap(bitmap);
-    }
-
-
-}
